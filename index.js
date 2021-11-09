3281f0e675d6281e12023a7763b8879de6e63dbb
Zach <zmommaerts@gmail.com>
2021-11-09 11:48:50 -0600

    commit tree 3086554cc985a9937a8a7a0200d2d43590e73a2d

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+const submitData = (userName, userEmail) => {
+    //function to add json object ID to the dom
+    const addToDom = (json) => {
+        const body = document.querySelector('body');
+        body.append(json);
+    }
+
+    //Info sent to post
+    const userInfo = {
+        name: userName,
+        email: userEmail
+    };
+
+    // JSON POST object
+    const sentObj = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify(userInfo)
+    };
+
+    //POST request to update databases and add json ID to the DOM
+    return fetch('http://localhost:3000/users', sentObj)
+    .then(res => res.json())
+    .then(json => addToDom(json.id))
+    .catch((error) => {
+        addToDom(error);
+    });
+}
+
+
